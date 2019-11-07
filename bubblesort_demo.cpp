0e47658989ba2d649a4acb6ffb740c316932f3af
Rohit singh <rohitsingh2410@gmail.com>
2019-11-07 09:45:49 +0530

    Create bubblesort_demo.cpp

---
+++ bubblesort_demo.cpp
@@ -0,0 +1,29 @@
+// Bubble sort demo
+// O(N)
+#include<iostream>
+using namespace std;
+int main() {
+
+    int data[5]={5,4,3,1,2};
+
+    for (int i = 0; i < 5-1; ++i)
+    {
+    	for (int j = i+1; j<=5; ++j)
+    	{
+    		if(data[i]>data[j]) //swap
+    		{
+    			int temp;
+    			temp=data[i];
+    			data[i]=data[j];
+    			data[j]=temp;
+    		}
+    	}
+    }
+
+    for (int i = 0; i < 5; ++i)
+    {
+    	cout<<data[i]<<" ";
+    }cout<<endl;
+
+	return 0;
+}
