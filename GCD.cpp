1d6f78c804f639ac5a5ed7f5a5da95ff7541b4ca
Rohit singh <rohitsingh2410@gmail.com>
2019-11-28 00:30:10 +0530

    Create GCD.cpp
    Program to find greatest common divisor of two number.

---
+++ GCD.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a,int b)
+{
+	if(a==0)
+	{
+		return b;
+	}else if(b==0)
+	{
+		return a;
+	}
+	else if(a>b)
+		return gcd(a%b,b);
+	else if(b>a)
+		return gcd(a,b%a);
+return 0;
+}
+
+
+int main()
+{
+
+int a,b;
+cin>>a>>b;
+
+cout<<gcd(a,b);
+	return 0;
+}
